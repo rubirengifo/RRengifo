16622a31a8cee379e915bc06680c039436324c1e
rubirengifo <106537961+rubirengifo@users.noreply.github.com>
2022-10-03 18:00:23 -0500

    mi examen

---
+++ src/main/java/com/rengifo/RRengifo/RRengifoApplication.java
@@ -0,0 +1,13 @@
+package com.rengifo.RRengifo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RRengifoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RRengifoApplication.class, args);
+	}
+
+}
